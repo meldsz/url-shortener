4364df86b716b0d14d6572181aa20a44740b8cb9
Meleesha <mayola.dsz@gmail.com>
2019-09-25 22:14:53 +0100

    mongoDB connection setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+require("./models/urlShortenerModel");
+
+const mongoURI = "mongodb://localhost/url-shortner";
+const connectOptions = {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+};
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, connectOptions, (err, db) => {
+  if (err) console.log(`Error`, err);
+  console.log(`Connected to MongoDB`);
+});
+
 const app = express();
 // access JSON data sent from client
 app.use(bodyParser.json());
