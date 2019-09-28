5c6b89088e3970697a6745acb13294f0bec5dbdf
Meleesha <mayola.dsz@gmail.com>
2019-09-28 14:04:31 +0100

    test for App component to mock api requests

--- src/components/root/App.test.js
+++ src/components/root/App.test.js
@@ -1,85 +1,63 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import App from './App';
-import sinon, { fakeServer } from 'sinon';
 import { url } from '../../data/fixtures';
 
-const inputUrl = 'http://dadaaaaa.com';
-const apiUrl = 'http://localhost:5000/api/urls';
-const shortUrl = "http://localhost:5000/lyMkFDYh";
+const inputUrl = 'http://url.com';
+const shortUrl = "http://localhost:5000/qwerty";
 
-describe('app', () => {
-  let server;
-  beforeEach(() => {
-    server = fakeServer.create();
-    server.respondWith('POST',
-      apiUrl,
-      [
-        200,
-        { 'Content-Type': 'application/json' },
-        JSON.stringify(url)
-      ]
-    );
+describe('app component', () => {
+  const app = mount(<App />);
+  const mockEvent = { target: { value: inputUrl } };
 
+  it('app renders correctly', () => {
+    expect(app).toMatchSnapshot();
   });
 
+  it('initial state', () => {
+    expect(app.state()).toEqual({ longUrl: '', shortUrl: '' });
+  });
 
-  describe('app component', () => {
-    const app = mount(<App />);
-    const mockEvent = { target: { value: inputUrl } };
+  it('does not render ShortUrl component initially', () => {
+    expect(app.find('ShortUrl').exists()).toBe(false);
+  });
 
-    it('app renders correctly', () => {
-      expect(app).toMatchSnapshot();
+  it('should have input box', () => {
+    expect(app.find('FormControl')).toHaveLength(1);
+  });
+
+  it('renders a Button `Submit` ', () => {
+    expect(app.find('.submit-btn').at(0).props().children).toEqual('Submit');
+  });
+
+  describe('updating the input box longUrl', () => {
+
+    beforeEach(() => {
+      app.find('FormControl').simulate('change', mockEvent);
+    })
+
+    it('update longUrl in the state', () => {
+      expect(app.state().longUrl).toEqual(inputUrl);
     });
 
-    it('initial state', () => {
-      expect(app.state()).toEqual({ longUrl: '', shortUrl: '' });
-    });
+    describe('submitting the form with longUrl', () => {
 
-    it('does not render ShortUrl component initially', () => {
-      expect(app.find('ShortUrl').exists()).toBe(false);
-    });
-
-    it('should have input box', () => {
-      expect(app.find('FormControl')).toHaveLength(1);
-    });
-
-    it('renders a Button `Submit` ', () => {
-      expect(app.find('.submit-btn').at(0).props().children).toEqual('Submit');
-    });
-
-    describe('updating the input box longUrl', () => {
-
-      beforeEach(() => {
-        app.find('FormControl').simulate('change', mockEvent);
-      })
-
-      it('update longUrl in the state', () => {
-        expect(app.state().longUrl).toEqual(inputUrl);
-      });
-
-      describe('submitting the form with longUrl', () => {
-
-        beforeEach(done => {
-          global.fetch = jest.fn().mockImplementation(() => {
-            return Promise.resolve(new Response(JSON.stringify(url)));
-          });
-          app.find('Button').simulate('click');
-          setTimeout(done)
+      beforeEach(done => {
+        global.fetch = jest.fn().mockImplementation(() => {
+          return Promise.resolve(new Response(JSON.stringify(url)));
         });
-
-        it('updates the state with shortUrl obtained from api', () => {
-          expect(global.fetch).toHaveBeenCalledTimes(1);
-          expect(app.state().shortUrl).toEqual(shortUrl);
-          global.fetch.mockClear();
-        })
+        app.find('Button').simulate('click');
+        setTimeout(done)
       });
 
+      it('updates the state with shortUrl obtained from api', () => {
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(app.state().shortUrl).toEqual(shortUrl);
+        global.fetch.mockClear();
+      });
     });
 
-
   });
-
 
 
 });
--- src/data/fixtures.js
+++ src/data/fixtures.js
@@ -1,15 +1,5 @@
-// export const url = {
-//     'shortUrl': 'http://localhost/jasgh',
-//     'originalUrl': 'http://url.com',
-//     'shortId': 'jasgh'
-// }
-
 export const url = {
-    "_id": "5d8b84d112592214d81b6304",
-    "originalUrl": "http://google.com",
-    "shortUrl": "http://localhost:5000/lyMkFDYh",
-    "shortId": "lyMkFDYh",
-    "mtime": "2019-09-25T15:16:33.710Z",
-    "ctime": "2019-09-25T15:16:33.814Z",
-    "__v": 0
+    'shortUrl': 'http://localhost:5000/qwerty',
+    'originalUrl': 'http://url.com',
+    'shortId': 'qwerty'
 }
