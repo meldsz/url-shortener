5e64f86f2292119bf26182bb9e59ad5af05aec6a
Meleesha <mayola.dsz@gmail.com>
2019-09-29 15:17:35 +0100

    added materialize css ui cdn

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,6 +1,6 @@
 export const host = "http://localhost:5000";
 
 export const endpoints = {
-    longUrl: `${host}/api/urls`,
-    getShortUrl: (shortId) => `${host}/api/urls/:${shortId}`
+    getUrls: `${host}/api/urls`,
+    getShortUrl: (shortId) => `${host}/api/urls/:${shortId}`,
 }
