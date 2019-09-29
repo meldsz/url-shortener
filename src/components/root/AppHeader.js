4d88d3a9f4e9ab4d49a164fd41856707af0c01f0
Meleesha <mayola.dsz@gmail.com>
2019-09-29 15:20:33 +0100

    added spinner for loading

---
+++ src/components/root/AppHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className="App-header">
+            <div className="link-icon">
+                <i className="fa fa-link"></i>
+            </div>
+
+            <div>
+                URL Shortener
+          </div>
+        </div>
+    );
+}
+
+export default AppHeader;
